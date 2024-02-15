6212b1ae08c1c93f72d671fadcaf8029bd6fb008
AWeirdScratcher <aweirdscratcher@gmail.com>
2024-02-15 20:35:13 +0800

    removed temp test

--- test.py
+++
@@ -1,9 +0,0 @@
-from websockets.sync.client import connect
-
-def hello():
-    with connect("ws://localhost:5000") as websocket:
-        websocket.send("Hello world!")
-        message = websocket.recv()
-        print(f"Received: {message}")
-
-hello()
